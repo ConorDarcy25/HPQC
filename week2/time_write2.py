d6a1b3931a8a224c7a8de1a8204d0d44df7e2370
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-02-09 18:42:06 +0000

    Update time_write2.py

--- week2/time_write2.py
+++ week2/time_write2.py
@@ -28,7 +28,8 @@ def main():
     run_time = end_time - start_time
     # prints the output
   #  ms = run_time.total_seconds() * 1000
-    ms = run_time.strftime("%S")
+    print("Time taken:".format(end_time-start_time))
+  #  ms = run_time.strftime("%S")
    
 
     fp.write("{ms}")
