5e09df2d2c5d4598ef3858df0173dd4c9fb81f92
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-02-10 20:35:59 +0000

    Update time_write.c

--- week2/time_write.c
+++ week2/time_write.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 {
 	// creates and initialises the variables
 	FILE *fptr;
-	ftpr = fopen("data/time_write.c","w");
+	fptr = fopen("data/time_write.c","w");
 	int i, input;
 	i = input = 0;
 	struct timespec start_time, end_time, time_diff;
