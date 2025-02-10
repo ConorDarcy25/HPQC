1d1b846d85c2ed3c0b956cdeb2613755ae4b3310
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-02-10 09:44:04 +0000

    Update time_write.py

--- week2/time_write.py
+++ week2/time_write.py
@@ -1,7 +1,8 @@
 import sys
 import time
+from datetime import datetime
 
-fp = open("data/time_py.txt", 'a')
+fp = open("data/time_py.txt", 'w')
 
 def main(): 
     output = 0
@@ -14,27 +15,27 @@ def main():
         raise Exception("Incorrect arguments.\nUsage: python time_print.py [NUM]\ne.g.\npython time_print.py 23")
 
     # gets the start time for the loop
-    start_time = time.time()
+    start_time = datetime.now()
     # iterates over all numbers up to the input
     for i in range(in_arg):
         # prints the index
        # fp.write(str(print("{}, ".format(i), end="")))
         fp.write(str(i))
     # gets the end time for the loop
-    end_time = time.time()
+    fp.write("\n")
+    end_time = datetime.now()
 
     # gets the total time
     run_time = end_time - start_time
     # prints the output
-
-    #print("\n\nTime for loop: {} seconds\n".format(run_time))
-
+  #  ms = run_time.total_seconds() * 1000
+    print("Time taken:{}".format(end_time-start_time))
+  #  ms = run_time.strftime("%S")
    
 
-    fp.write(str(run_time.time()*1000.0))
+    fp.write("Time taken:{}".format(end_time-start_time))
     fp.close
 
 
 if __name__ == "__main__":
     main()
-
