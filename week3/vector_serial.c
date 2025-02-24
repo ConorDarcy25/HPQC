639cad1339eb5e337746f6e9b0a0d11272bc5bfa
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-02-24 16:56:13 +0000

    Updated to be non-trivial

--- week3/vector_serial.c
+++ week3/vector_serial.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // declares the functions that will be called within main
 // note how declaration lines are similar to the initial line
 // of a function definition, but with a semicolon at the end;
+double to_second_float(struct timespec in_time);
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time);
 int check_args(int argc, char **argv);
 void initialise_vector(int vector[], int size, int initial);
 void print_vector(int vector[], int size);
 int sum_vector(int vector[], int size);
+void non_trivial_vector(int vector[], int size);
+
 
 int main(int argc, char **argv)
 {
 	// declare and initialise the numerical argument variable
 	int num_arg = check_args(argc, argv);
+	
+	struct timespec start_time, end_time, time_diff;
+	double runtime = 0.0;
+	// get time before the sum 
+	timespec_get(&start_time, TIME_UTC);
+
 
 	// creates a vector variable
 	// int my_vector[num_arg]; // suffers issues for large vectors
@@ -22,12 +33,24 @@ int main(int argc, char **argv)
 
 
 	// TODO: put some code here that makes a more meaningful vector
+	non_trivial_vector(my_vector, num_arg);
+
 
 	// sums the vector
 	int my_sum = sum_vector(my_vector, num_arg);
+	// get time at the end of the sum
+        timespec_get(&end_time, TIME_UTC);
+
+	// calculates the runtime
+	time_diff = calculate_runtime(start_time, end_time);
+	runtime = to_second_float(time_diff);
 
 	// prints the sum
 	printf("Sum: %d\n", my_sum);
+
+	// outputs the runtime
+	printf("\n\nRuntime for core loop: %lf seconds.\n\n", runtime);
+
 
 	// if we use malloc, must free when done!
 	free(my_vector);
@@ -63,6 +86,18 @@ void initialise_vector(int vector[], int size, int initial)
 	}
 }
 
+// defines a function to initialise all values in a vector to a given inital value
+void non_trivial_vector(int vector[], int size)
+{
+	// iterates through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// sets the elements of the vector to the initial value
+		vector[i] = i+2;
+	}
+}
+
+
 // defines a function to print a vector of ints
 void print_vector(int vector[], int size)
 {
@@ -97,4 +132,46 @@ int check_args(int argc, char **argv)
 	}
 	return num_arg;
 }
+double to_second_float(struct timespec in_time)
+{
+	// creates and initialises the variables
+	float out_time = 0.0;
+	long int seconds, nanoseconds;
+	seconds = nanoseconds = 0;
 
+	// extracts the elements from in_time
+	seconds = in_time.tv_sec;
+	nanoseconds = in_time.tv_nsec;
+
+	// calculates the time in seconds by adding the seconds and the nanoseconds divided by 1e9
+	out_time = seconds + nanoseconds/1e9;
+
+	// returns the time as a double
+	return out_time;
+}
+
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time)
+{
+	// creates and initialises the variables
+	struct timespec time_diff;
+	long int seconds, nanoseconds;                                                                                                       seconds = nanoseconds = 0;
+	double runtime = 0.0;
+
+	// extracts the elements from start_time and end_time
+	seconds = end_time.tv_sec - start_time.tv_sec;
+	nanoseconds = end_time.tv_nsec - start_time.tv_nsec;
+
+	// if the ns part is negative
+	if (nanoseconds < 0)
+	{
+		// "carry the one!"
+		seconds = seconds - 1;
+		nanoseconds = ((long int) 1e9) - nanoseconds;
+	}
+
+	// creates the runtime
+	time_diff.tv_sec = seconds;
+	time_diff.tv_nsec = nanoseconds;
+
+	return time_diff;
+}
