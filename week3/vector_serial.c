8842e4bdb55ca00db7423e8e0b0a107c8dbd3945
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-03 15:05:28 +0000

    Update vector_serial.c

--- week3/vector_serial.c
+++ week3/vector_serial.c
@@ -1,177 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <mpi.h>
+#include <string.h>
 
-// declares the functions that will be called within main
-// note how declaration lines are similar to the initial line
-// of a function definition, but with a semicolon at the end;
-double to_second_float(struct timespec in_time);
-struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time);
-int check_args(int argc, char **argv);
-void initialise_vector(int vector[], int size, int initial);
-void print_vector(int vector[], int size);
-int sum_vector(int vector[], int size);
-void non_trivial_vector(int vector[], int size);
+// Functions
+int sum_vector(int* vector, int size);
+void init_vector(int* vector, int size);
 
+int main(int argc, char **argv) {
+    int rank, num_procs, total_size, local_size;
+    int *vector = NULL, *local_vector = NULL;
+    int local_sum = 0, total_sum = 0;
+    int time_flag = 0;
 
-int main(int argc, char **argv)
-{
-	// declare and initialise the numerical argument variable
-	int num_arg = check_args(argc, argv);
-	
-	struct timespec start_time, end_time, time_diff;
-	double runtime = 0.0;
-	// get time before the sum 
-	timespec_get(&start_time, TIME_UTC);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
+    if (rank == 0) {
+        if (argc < 2) {
+            printf("Usage: %s <size> [time]\n", argv[0]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        total_size = atoi(argv[1]);
+        if (argc > 2 && strcmp(argv[2], "time") == 0) {
+            time_flag = 1;
+        }
+        vector = malloc(total_size * sizeof(int));
+        if (vector == NULL) {
+            printf("Memory allocation failed\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        init_vector(vector, total_size);
+    }
 
-	// creates a vector variable
-	// int my_vector[num_arg]; // suffers issues for large vectors
-	int* my_vector = malloc (num_arg * sizeof(int));
-	// and initialises every element to zero
-	initialise_vector(my_vector, num_arg, 0);
+    MPI_Bcast(&total_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&time_flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    local_size = total_size / num_procs;
+    local_vector = malloc(local_size * sizeof(int));
+    MPI_Scatter(vector, local_size, MPI_INT, local_vector, local_size, MPI_INT, 0, MPI_COMM_WORLD);
 
+    double start_time, end_time;
+    if (time_flag) {
+        start_time = MPI_Wtime();
+    }
 
-	// TODO: put some code here that makes a more meaningful vector
-	non_trivial_vector(my_vector, num_arg);
+    local_sum = sum_vector(local_vector, local_size);
+    MPI_Reduce(&local_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+    if (time_flag) {
+        end_time = MPI_Wtime();
+    }
 
-	// sums the vector
-	int my_sum = sum_vector(my_vector, num_arg);
-	// get time at the end of the sum
-        timespec_get(&end_time, TIME_UTC);
+    if (rank == 0) {
+        printf("Total Sum: %d\n", total_sum);
+        if (time_flag) {
+            printf("Execution Time: %lf seconds\n", end_time - start_time);
+        }
+        free(vector);
+    }
+    free(local_vector);
 
-	// calculates the runtime
-	time_diff = calculate_runtime(start_time, end_time);
-	runtime = to_second_float(time_diff);
-
-	// prints the sum
-	printf("Sum: %d\n", my_sum);
-
-	// outputs the runtime
-	printf("\n\nRuntime for core loop: %lf seconds.\n\n", runtime);
-
-
-	// if we use malloc, must free when done!
-	free(my_vector);
-
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
 
-// defines a function to sum a vector of ints into another int
-int sum_vector(int vector[], int size)
-{
-	// creates a variable to hold the sum
-	int sum = 0;
-
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// sets the elements of the vector to the initial value
-		sum += vector[i];
-	}
-
-	// returns the sum
-	return sum;
+int sum_vector(int* vector, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += vector[i];
+    }
+    return sum;
 }
 
-// defines a function to initialise all values in a vector to a given inital value
-void initialise_vector(int vector[], int size, int initial)
-{
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// sets the elements of the vector to the initial value
-		vector[i] = initial;
-	}
-}
-
-// defines a function to initialise all values in a vector to a given inital value
-void non_trivial_vector(int vector[], int size)
-{
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// sets the elements of the vector to the initial value
-		vector[i] = i+2;
-	}
-}
-
-
-// defines a function to print a vector of ints
-void print_vector(int vector[], int size)
-{
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// prints the elements of the vector to the screen
-		printf("%d\n", vector[i]);
-	}
-}
-
-// defines a function that checks your arguments to make sure they'll do what you need
-int check_args(int argc, char **argv)
-{
-	// declare and initialise the numerical argument
-	int num_arg = 0;
-
-	// check the number of arguments
-	if (argc == 2) // program name and numerical argument
-	{
-		// declare and initialise the numerical argument
-		num_arg = atoi(argv[1]);
-	}
-	else // the number of arguments is incorrect
-	{
-		// raise an error
-		fprintf(stderr, "ERROR: You did not provide a numerical argument!\n");
-		fprintf(stderr, "Correct use: %s [NUMBER]\n", argv[0]);
-
-		// and exit COMPLETELY
-		exit (-1);
-	}
-	return num_arg;
-}
-double to_second_float(struct timespec in_time)
-{
-	// creates and initialises the variables
-	float out_time = 0.0;
-	long int seconds, nanoseconds;
-	seconds = nanoseconds = 0;
-
-	// extracts the elements from in_time
-	seconds = in_time.tv_sec;
-	nanoseconds = in_time.tv_nsec;
-
-	// calculates the time in seconds by adding the seconds and the nanoseconds divided by 1e9
-	out_time = seconds + nanoseconds/1e9;
-
-	// returns the time as a double
-	return out_time;
-}
-
-struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time)
-{
-	// creates and initialises the variables
-	struct timespec time_diff;
-	long int seconds, nanoseconds;                                                                                                       seconds = nanoseconds = 0;
-	double runtime = 0.0;
-
-	// extracts the elements from start_time and end_time
-	seconds = end_time.tv_sec - start_time.tv_sec;
-	nanoseconds = end_time.tv_nsec - start_time.tv_nsec;
-
-	// if the ns part is negative
-	if (nanoseconds < 0)
-	{
-		// "carry the one!"
-		seconds = seconds - 1;
-		nanoseconds = ((long int) 1e9) - nanoseconds;
-	}
-
-	// creates the runtime
-	time_diff.tv_sec = seconds;
-	time_diff.tv_nsec = nanoseconds;
-
-	return time_diff;
+void init_vector(int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = i + 1;
+    }
 }
