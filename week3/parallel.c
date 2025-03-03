0a404e8ea33618368f7ca3123da8b383906d732c
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-03 15:16:42 +0000

    Update parallel.c

--- week3/parallel.c
+++ week3/parallel.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <mpi.h>
 
-
+// Functions
 int sum_vector(int* vector, int size);
 void init_vector(int* vector, int size);
-double to_seconds(struct timespec t);
 
 int main(int argc, char **argv) {
     int rank, num_procs, total_size, local_size;
     int *vector = NULL, *local_vector = NULL;
     int local_sum = 0, total_sum = 0;
-    struct timespec start_time, end_time;
-    double runtime;
+    int time_flag = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -21,10 +18,19 @@ int main(int argc, char **argv) {
 
     if (rank == 0) {
         if (argc < 2) {
-            printf("Usage: %s <size>\n", argv[0]);
+            printf("Usage: %s <size> [time]\n", argv[0]);
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
         total_size = atoi(argv[1]);
+        if (argc > 2) {
+            for (int i = 0; argv[2][i] != '\0'; i++) {
+                if (argv[2][i] != "time"[i]) {
+                    time_flag = 0;
+                    break;
+                }
+                time_flag = 1;
+            }
+        }
         vector = malloc(total_size * sizeof(int));
         if (vector == NULL) {
             printf("Memory allocation failed\n");
@@ -34,19 +40,28 @@ int main(int argc, char **argv) {
     }
 
     MPI_Bcast(&total_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&time_flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
     local_size = total_size / num_procs;
     local_vector = malloc(local_size * sizeof(int));
     MPI_Scatter(vector, local_size, MPI_INT, local_vector, local_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    timespec_get(&start_time, TIME_UTC);
+    double start_time, end_time;
+    if (time_flag) {
+        start_time = MPI_Wtime();
+    }
+
     local_sum = sum_vector(local_vector, local_size);
     MPI_Reduce(&local_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    timespec_get(&end_time, TIME_UTC);
+
+    if (time_flag) {
+        end_time = MPI_Wtime();
+    }
 
     if (rank == 0) {
-        runtime = to_seconds(end_time) - to_seconds(start_time);
         printf("Total Sum: %d\n", total_sum);
-        printf("Execution Time: %lf seconds\n", runtime);
+        if (time_flag) {
+            printf("Execution Time: %lf seconds\n", end_time - start_time);
+        }
         free(vector);
     }
     free(local_vector);
@@ -67,8 +82,4 @@ void init_vector(int* vector, int size) {
     for (int i = 0; i < size; i++) {
         vector[i] = i + 1;
     }
-}
-
-double to_seconds(struct timespec t) {
-    return t.tv_sec + (t.tv_nsec / 1e9);
 }
