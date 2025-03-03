f89e8613dd5897f4eebef9b30d52610fa9ad9121
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-03 13:34:56 +0000

    Update parallel.c

--- week3/parallel.c
+++ week3/parallel.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 
 // Function declarations
 double to_second_float(struct timespec in_time);
@@ -25,7 +24,7 @@ int main(int argc, char **argv)
     initialise_vector(my_vector, num_arg, 0);
     non_trivial_vector(my_vector, num_arg);
 
-    int num_threads = omp_get_max_threads(); // Use max available threads
+    int num_threads = 4; // Set a fixed number of threads
     int my_sum = parallel_sum_vector(my_vector, num_arg, num_threads);
 
     timespec_get(&end_time, TIME_UTC);
@@ -43,17 +42,28 @@ int main(int argc, char **argv)
 int parallel_sum_vector(int vector[], int size, int num_threads)
 {
     int total_sum = 0;
-    #pragma omp parallel num_threads(num_threads)
-    {
-        int thread_sum = 0;
-        #pragma omp for nowait
-        for (int i = 0; i < size; i++)
-        {
-            thread_sum += vector[i];
-        }
-        #pragma omp atomic
-        total_sum += thread_sum;
+    int chunk_size = size / num_threads;
+    int remainder = size % num_threads;
+    
+    int* partial_sums = malloc(num_threads * sizeof(int));
+    for (int i = 0; i < num_threads; i++) {
+        partial_sums[i] = 0;
     }
+    
+    for (int t = 0; t < num_threads; t++) {
+        int start = t * chunk_size;
+        int end = (t == num_threads - 1) ? (start + chunk_size + remainder) : (start + chunk_size);
+        
+        for (int i = start; i < end; i++) {
+            partial_sums[t] += vector[i];
+        }
+    }
+    
+    for (int i = 0; i < num_threads; i++) {
+        total_sum += partial_sums[i];
+    }
+    
+    free(partial_sums);
     return total_sum;
 }
 
