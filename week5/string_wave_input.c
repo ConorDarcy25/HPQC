5d673f4837b8c63d89d059c06f27d4ed5fd552e3
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-09 18:40:09 +0000

    Update string_wave_input.c

--- week5/string_wave_input.c
+++ week5/string_wave_input.c
@@ -1,187 +1,107 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-// declares the functions that will be called within main
-// note how declaration lines are similar to the initial line
-// of a function definition, but with a semicolon at the end;
-int check_args(int argc, char **argv);
+// Struct to store command-line arguments
+typedef struct {
+    int points;
+    int cycles;
+    int samples;
+    char output_file[256];
+} Args;
+
+int check_args(int argc, char **argv, Args *args);
 void initialise_vector(double vector[], int size, double initial);
-void print_vector(double vector[], int size);
-int sum_vector(int vector[], int size);
+void print_header(FILE** p_out_file, int points);
 void update_positions(double* positions, int points, double time);
 int generate_timestamps(double* time_stamps, int time_steps, double step_size);
 double driver(double time);
-void print_header(FILE** p_out_file, int points);
 
-int main(int argc, char **argv)
-{
-	// declare and initialise the numerical argument variable
-	int points = check_args(argc, argv);
+int main(int argc, char **argv) {
+    // Parse arguments
+    Args args;
+    check_args(argc, argv, &args);
 
-	// creates variables for the vibration
-	int cycles = 5; // number of cycles to show
-	int samples = 25; // sampling rate in samples per cycle
-	int time_steps = cycles * samples + 1; // total timesteps
-	double step_size = 1.0/samples;
+    int time_steps = args.cycles * args.samples + 1;
+    double step_size = 1.0 / args.samples;
 
-	// creates a vector for the time stamps in the data
-	double* time_stamps = (double*) malloc(time_steps * sizeof(double));
-	initialise_vector(time_stamps, time_steps, 0.0);
-	generate_timestamps(time_stamps, time_steps, step_size);
+    // Allocate memory for timestamps and positions
+    double* time_stamps = (double*) malloc(time_steps * sizeof(double));
+    initialise_vector(time_stamps, time_steps, 0.0);
+    generate_timestamps(time_stamps, time_steps, step_size);
 
-	// creates a vector variable for the current positions
-	double* positions = (double*) malloc(points * sizeof(double));
-	// and initialises every element to zero
-	initialise_vector(positions, points, 0.0);
+    double* positions = (double*) malloc(args.points * sizeof(double));
+    initialise_vector(positions, args.points, 0.0);
 
-	// creates a file
-	FILE* out_file;
-     	out_file = fopen("data/string_wave.csv","w");
-	print_header(&out_file, points);
+    // Open file for writing
+    FILE* out_file = fopen(args.output_file, "w");
+    if (!out_file) {
+        fprintf(stderr, "ERROR: Unable to open file %s for writing.\n", args.output_file);
+        exit(EXIT_FAILURE);
+    }
+    print_header(&out_file, args.points);
 
-	// iterates through each time step in the collection
-	for (int i = 0; i < time_steps; i++)
-	{
-		// updates the position using a function
-		update_positions(positions, points, time_stamps[i]);
-
-		// prints an index and time stamp
-		fprintf(out_file, "%d, %lf", i, time_stamps[i]);
-
-		// iterates over all of the points on the line
-		for (int j = 0; j < points; j++)
-		{
-			// prints each y-position to a file
-			fprintf(out_file, ", %lf", positions[j]);
-		}
-		// prints a new line
-		fprintf(out_file, "\n");
-	}
-
-	// if we use malloc, must free when done!
-	free(time_stamps);
-	free(positions);
-
-	// closes the file
-	fclose(out_file);
-
-	return 0;
-}
-
-// prints a header to the file
-// double-pointer used to allow this function to move the file pointer
-void print_header(FILE** p_out_file, int points)
-{
-	fprintf(*p_out_file, "#, time");
-	for (int j = 0; j < points; j++)
-	{
-		fprintf(*p_out_file, ", y[%d]", j);
-	}
-	fprintf(*p_out_file, "\n");
-}
-
-// defines a simple harmonic oscillator as the driving force
-double driver(double time)
-{
-	double value = sin(time*2.0*M_PI);
-	return(value);
-}
-
-// defines a function to update the positions
-void update_positions(double* positions, int points, double time)
-{
-	// creates a temporary vector variable for the new positions
-        double* new_positions = (double*) malloc(points * sizeof(double));
-
-	// initialises the index
-	int i = 0;
-	new_positions[i] = driver(time);
-	// creates new positions by setting value of previous element 
-	for (i = 1; i < points; i++)
-	{
-		new_positions[i] = positions[i-1];
-	}
-	// propagates these new positions to the old ones
-	for (i = 0; i < points; i++)
-        {
-                positions[i] = new_positions[i];
+    for (int i = 0; i < time_steps; i++) {
+        update_positions(positions, args.points, time_stamps[i]);
+        fprintf(out_file, "%d, %lf", i, time_stamps[i]);
+        for (int j = 0; j < args.points; j++) {
+            fprintf(out_file, ", %lf", positions[j]);
         }
+        fprintf(out_file, "\n");
+    }
 
-	// frees the temporary vector
-	free(new_positions);
+    free(time_stamps);
+    free(positions);
+    fclose(out_file);
+    return 0;
 }
 
-// defines a set of timestamps
-int generate_timestamps(double* timestamps, int time_steps, double step_size)
-{
-	for (int i = 0; i < time_steps ; i++)
-	{
-		timestamps[i]=i*step_size;
-	}	
-	return time_steps;
+int check_args(int argc, char **argv, Args *args) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s points cycles samples output_file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    args->points = atoi(argv[1]);
+    args->cycles = atoi(argv[2]);
+    args->samples = atoi(argv[3]);
+    strncpy(args->output_file, argv[4], 255);
+    args->output_file[255] = '\0';
+    return 0;
 }
 
-// defines a function to sum a vector of ints into another int
-int sum_vector(int vector[], int size)
-{
-	// creates a variable to hold the sum
-	int sum = 0;
-
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// sets the elements of the vector to the initial value
-		sum += vector[i];
-	}
-
-	// returns the sum
-	return sum;
+void print_header(FILE** p_out_file, int points) {
+    fprintf(*p_out_file, "#, time");
+    for (int j = 0; j < points; j++) {
+        fprintf(*p_out_file, ", y[%d]", j);
+    }
+    fprintf(*p_out_file, "\n");
 }
 
-// defines a function to initialise all values in a vector to a given inital value
-void initialise_vector(double vector[], int size, double initial)
-{
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// sets the elements of the vector to the initial value
-		vector[i] = initial;
-	}
+double driver(double time) {
+    return sin(time * 2.0 * M_PI);
 }
 
-// defines a function to print a vector of ints
-void print_vector(double vector[], int size)
-{
-	// iterates through the vector
-	for (int i = 0; i < size; i++)
-	{
-		// prints the elements of the vector to the screen
-		printf("%d, %lf\n", i, vector[i]);
-	}
+void update_positions(double* positions, int points, double time) {
+    double* new_positions = (double*) malloc(points * sizeof(double));
+    new_positions[0] = driver(time);
+    for (int i = 1; i < points; i++) {
+        new_positions[i] = positions[i - 1];
+    }
+    memcpy(positions, new_positions, points * sizeof(double));
+    free(new_positions);
 }
 
-// defines a function that checks your arguments to make sure they'll do what you need
-int check_args(int argc, char **argv)
-{
-	// declare and initialise the numerical argument
-	int num_arg = 0;
+int generate_timestamps(double* timestamps, int time_steps, double step_size) {
+    for (int i = 0; i < time_steps; i++) {
+        timestamps[i] = i * step_size;
+    }
+    return time_steps;
+}
 
-	// check the number of arguments
-	if (argc == 2) // program name and numerical argument
-	{
-		// declare and initialise the numerical argument
-		num_arg = atoi(argv[1]);
-	}
-	else // the number of arguments is incorrect
-	{
-		// raise an error
-		fprintf(stderr, "ERROR: You did not provide a numerical argument!\n");
-		fprintf(stderr, "Correct use: %s [NUMBER]\n", argv[0]);
-
-		// and exit COMPLETELY
-		exit (-1);
-	}
-	return num_arg;
+void initialise_vector(double vector[], int size, double initial) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = initial;
+    }
 }
