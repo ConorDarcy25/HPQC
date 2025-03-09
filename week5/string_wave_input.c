cad899b56cc5a6e169b128e03887a784c0219578
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-09 19:26:27 +0000

    Update string_wave_input.c

--- week5/string_wave_input.c
+++ week5/string_wave_input.c
@@ -1,51 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
-// Struct to store command-line arguments
-typedef struct {
-    int points;
-    int cycles;
-    int samples;
-    char string_file[256];
-} Args;
+// Global variables instead of struct
+int points;
+int cycles;
+int samples;
 
-int check_args(int argc, char **argv, Args *args);
 void initialise_vector(double vector[], int size, double initial);
-void print_header(FILE** p_out_file, int points);
+void print_header(FILE* p_out_file, int points);
 void update_positions(double* positions, int points, double time);
 int generate_timestamps(double* time_stamps, int time_steps, double step_size);
 double driver(double time);
 
 int main(int argc, char **argv) {
-    // Parse arguments
-    Args args;
-    check_args(argc, argv, &args);
+    // Basic argument parsing without struct
+    if (argc != 4) {
+        printf("Usage: %s points cycles samples\n", argv[0]);
+        return 1;
+    }
+    points = atoi(argv[1]);
+    cycles = atoi(argv[2]);
+    samples = atoi(argv[3]);
 
-    int time_steps = args.cycles * args.samples + 1;
-    double step_size = 1.0 / args.samples;
+    int time_steps = cycles * samples + 1;
+    double step_size = 1.0 / samples;
 
-    // Allocate memory for timestamps and positions
-    double* time_stamps = (double*) malloc(time_steps * sizeof(double));
+    double* time_stamps = malloc(time_steps * sizeof(double));
+    if (time_stamps == NULL) {
+        printf("Memory allocation failed for time_stamps\n");
+        return 1;
+    }
     initialise_vector(time_stamps, time_steps, 0.0);
     generate_timestamps(time_stamps, time_steps, step_size);
 
-    double* positions = (double*) malloc(args.points * sizeof(double));
-    initialise_vector(positions, args.points, 0.0);
-
-    // Open file for writing
-    FILE* out_file = fopen(args.string_file, "w");
-    if (!out_file) {
-        fprintf(stderr, "ERROR: Unable to open file %s for writing.\n", args.string_file);
-        exit(EXIT_FAILURE);
+    double* positions = malloc(points * sizeof(double));
+    if (positions == NULL) {
+        printf("Memory allocation failed for positions\n");
+        free(time_stamps);
+        return 1;
     }
-    print_header(&out_file, args.points);
+    initialise_vector(positions, points, 0.0);
+
+    FILE* out_file = fopen("string_wave.csv", "w");
+    if (out_file == NULL) {
+        printf("Error opening file\n");
+        free(time_stamps);
+        free(positions);
+        return 1;
+    }
+    print_header(out_file, points);
 
     for (int i = 0; i < time_steps; i++) {
-        update_positions(positions, args.points, time_stamps[i]);
+        update_positions(positions, points, time_stamps[i]);
         fprintf(out_file, "%d, %lf", i, time_stamps[i]);
-        for (int j = 0; j < args.points; j++) {
+        for (int j = 0; j < points; j++) {
             fprintf(out_file, ", %lf", positions[j]);
         }
         fprintf(out_file, "\n");
@@ -57,26 +66,12 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-int check_args(int argc, char **argv, Args *args) {
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s points cycles samples string_file\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    args->points = atoi(argv[1]);
-    args->cycles = atoi(argv[2]);
-    args->samples = atoi(argv[3]);
-    strncpy(args->string_file, argv[4], 255);
-    args->string_file[255] = '\0';
-    return 0;
-}
-
-void print_header(FILE** p_out_file, int points) {
-    fprintf(*p_out_file, "#, time");
+void print_header(FILE* p_out_file, int points) {
+    fprintf(p_out_file, "#, time");
     for (int j = 0; j < points; j++) {
-        fprintf(*p_out_file, ", y[%d]", j);
+        fprintf(p_out_file, ", y[%d]", j);
     }
-    fprintf(*p_out_file, "\n");
+    fprintf(p_out_file, "\n");
 }
 
 double driver(double time) {
@@ -84,12 +79,18 @@ double driver(double time) {
 }
 
 void update_positions(double* positions, int points, double time) {
-    double* new_positions = (double*) malloc(points * sizeof(double));
+    double* new_positions = malloc(points * sizeof(double));
+    if (new_positions == NULL) {
+        printf("Memory allocation failed for new_positions\n");
+        return;
+    }
     new_positions[0] = driver(time);
     for (int i = 1; i < points; i++) {
         new_positions[i] = positions[i - 1];
     }
-    memcpy(positions, new_positions, points * sizeof(double));
+    for (int i = 0; i < points; i++) {
+        positions[i] = new_positions[i];
+    }
     free(new_positions);
 }
 
