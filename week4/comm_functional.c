f5ee2b21ff83d5cc274e947ea565ec67782d49cc
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-09 15:52:37 +0000

    Update comm_functional.c

--- week4/comm_functional.c
+++ week4/comm_functional.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <mpi.h>
 
-// Function prototypes
 void main_task(int uni_size, int source, int rank);
 void client_task(int uni_size, int my_rank);
 void run_tasks(int uni_size, int my_rank);
@@ -9,18 +8,18 @@ void check_processors(int uni_size, int my_rank);
 
 int main(int argc, char **argv) 
 {
-    // declare and initialise rank and size variables
+    // Initalise rank and variable
     int my_rank, uni_size;
     
-    // initialize MPI
+    // Initialize MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
     
-    // checks the number of processors and runs tasks if valid
+    // Checks the number of processors and runs tasks if valid
     check_processors(uni_size, my_rank);
     
-    // finalize MPI
+    // Finalize MPI
     MPI_Finalize();
     return 0;
 }
@@ -30,10 +29,10 @@ void main_task(int uni_size, int source, int rank)
     int recv_message, count = 1, tag = 0;
     MPI_Status status;
     
-    // receives the message
+    // Receives the message
     MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
     
-    // prints the message from the sender
+    // prints the message
     printf("Hello, I am %d of %d. Received %d from Rank %d\n", rank, uni_size, recv_message, source);
 }
 
@@ -42,10 +41,10 @@ void client_task(int uni_size, int my_rank)
     int send_message = my_rank * 10;
     int dest = 0, count = 1, tag = 0;
     
-    // sends the message
+    // Sends the message
     MPI_Send(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
     
-    // prints the message from the sender
+    // prints the message
     printf("Hello, I am %d of %d. Sent %d to Rank %d\n", my_rank, uni_size, send_message, dest);
 }
 
@@ -65,7 +64,7 @@ void check_processors(int uni_size, int my_rank)
     if (uni_size > 1) {
         run_tasks(uni_size, my_rank);
     } else {
-        // prints a warning
-        printf("Unable to communicate with less than 2 processes. MPI communicator size = %d\n", uni_size);
+        // prints a warning if less than 2 processes are entered
+        printf("Cannot run with less than 2 processes. MPI communicator size = %d\n", uni_size);
     }
 }
