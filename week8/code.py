aea775946625c266bcd20edfdb32131e18979651
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-04-13 13:56:46 +0100

    Update code.py

--- week8/code.py
+++ week8/code.py
@@ -1,1 +1,44 @@
 # Grover's search
+
+# Subroutines:
+
+def zero_booleanOracle(qubits,result): # all qubits zero? 
+    # if all qubits==0 return 1 else return 0
+    for qubit in qubits:             # negate all inputs
+        applyGate(X_gate,qubit)
+    TOFFn_gate(qubits,result)        # compute AND
+    for qubit in qubits:             # restore inputs
+        applyGate(X_gate,qubit)
+
+def zero_phaseOracle(qubits):            # all qubits zero? 
+    # if all qubits==0 return -weight else return weight
+    for qubit in qubits:                 # negate all inputs
+        applyGate(X_gate,qubit)
+    applyGate(Z_gate,*namestack)         # controlled Z gate
+    for qubit in qubits:                 # restore inputs
+        applyGate(X_gate,qubit)
+
+def sample_phaseOracle(qubits):          # sample function 
+        # if all f(x)==1 return -weight else return weight
+    applyGate(X_gate,qubits[1])          # negate qubit 1
+    applyGate(Z_gate,*namestack)         # controlled Z gate
+    applyGate(X_gate,qubits[1])          # restore qubit 1
+
+# The Main Loop:
+
+def groverSearch(n, printProb=True):
+    optimalTurns = int(np.pi/4*np.sqrt(2**n)-1/2)   # iterations 
+    qubits = list(range(n))                         # generate qubit names
+    for qubit in qubits:                            # initialize qubits
+        pushQubit(qubit,[1,1])
+    for k in range(optimalTurns):                   # Grover iterations:
+        sample_phaseOracle(qubits)                  # apply phase oracle
+        for qubit in qubits:                        # H-gate all qubits
+            applyGate(H_gate,qubit) 
+        zero_phaseOracle(qubits)                    # apply 0 phase oracle
+        for qubit in qubits:                        # H-gate all qubits
+            applyGate(H_gate,qubit) 
+        if printProb:                               # peek probabilities
+            print(probQubit(qubits[0]))             # to show convergence
+    for qubit in reversed(qubits):                  # print result 
+        print(measureQubit(qubit),end="")
